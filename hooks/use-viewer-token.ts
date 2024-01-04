bd078548fb86d021d07b84fb416aa8e196f6c638
Duc Thai <ducthai060501@gmail.com>
2024-01-04 13:11:30 +0200

    view token functionality

---
+++ hooks/use-viewer-token.ts
@@ -0,0 +1,41 @@
+import { toast } from "sonner";
+import { useEffect, useState } from "react";
+import { JwtPayload, jwtDecode } from "jwt-decode";
+import { createViewerToken } from "@/actions/token";
+export const useViewerToken = (hostIdetity: string) => {
+  const [token, setToken] = useState("");
+  const [name, setName] = useState("");
+  const [identity, setIdentity] = useState("");
+
+  useEffect(() => {
+    const createToken = async () => {
+      try {
+        const viewerToken = await createViewerToken(hostIdetity);
+
+        setToken(viewerToken);
+
+        const decodedToken = jwtDecode(viewerToken) as JwtPayload & {
+          name?: string;
+        };
+
+        const name = decodedToken?.name;
+        const identity = decodedToken.jti;
+
+        if (identity) {
+          setIdentity(identity);
+        }
+
+        if (name) {
+          setName(name);
+        }
+      } catch (error) {
+        toast.error("Something went wrong");
+      }
+    };
+    createToken()
+  }, [hostIdetity]);
+
+  return {
+    token, name, identity
+  }
+};
